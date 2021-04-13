e73eb41a41b7d58b3ab22a2233fbb2fd94414c47
iloverugs <81831508+iloverugs@users.noreply.github.com>
2021-04-12 20:17:12 -0400

    Started stars assignemnt

---
+++ projects/exercises/stars/stars.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.sprite import Sprite 
+
+class Star(Sprite):
+    """A class to represent an individual star in the grid."""
+
+    def __init__(self, star_grid):
+        """Initialize the star and its location."""
+        super().__init__()
+        self.screen = star_grid.screen
+
+        # Load the star image and set its rect attributes
+        self.image = pygame.image.load('images/star.jpg')
+        self.rect = self.image.get_rect()
+
+        # Put first star near top left of the screen
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the alien's exact horizontal position.
+        self.x = float(self.rectx)
